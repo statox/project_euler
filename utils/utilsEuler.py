4e5283e7c210da1356147e7b2d339ae98e583923
statox <adrien.fabre92@gmail.com>
2016-12-04 15:43:47 +0100

    utilsEuler: add isPrime()

--- utils/utilsEuler.py
+++ utils/utilsEuler.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
-# 73,79,83,89,97,101,103, 107,109,113,127,131,137,139,149,
-# 151,157,163,167,173,179,181,191,193,197,199,211,223,227,
-# 229,233,239,241,251,257,263,269,271,277,281,283,293,307,
-# 311,313,317,331,337,347, 349,353,359,367,373,379,383,389,
-# 397,401,409,419,421,431,433,439,443,449,457,461,463,467,
-# 479,487,491,499,503,509,521,523,541
+import math
 
 # Create a generator for the primes numbers under limit
 # The generator can be used only once
@@ -26,6 +19,15 @@ def eratosthenesGenerator(limit):
 # Returs a list of the primes numbers under limit
 def eratosthenesList(limit):
     return [i for i in eratosthenesGenerator(limit)]
+
+
+# Test if a number is prime by trial division
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if (n%i == 0):
+            return False
+
+    return True
 
 # Test if a string is a palindromes
 def isPalindrome(n):
