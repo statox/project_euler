1ca3204c4497723b23de7ea43b22021933378cb6
statox <adrien.fabre92@gmail.com>
2016-12-14 20:35:33 +0100

    utilsEuler: Add permute()

--- utils/utilsEuler.py
+++ utils/utilsEuler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import math
+import re
 
 # Create a generator for the primes numbers under limit
 # The generator can be used only once
@@ -39,3 +40,19 @@ def isPalindrome(n):
 # Return the nth triangular number
 def triangular(n):
     return n * (n + 1)/2
+
+# Recursively find permutations of a string
+def permute(n):
+    if (len(n) == 2):
+        return [str(n), str(n[1]+n[0])]
+    else:
+        list = []
+        for i in n:
+            sub = permute(re.sub(i, '', n))
+
+            for s in sub:
+                list.append(i + s)
+                if (len(list) % 1000000 == 0):
+                    print "+1000000 permutations: " + i+s
+
+        return list
