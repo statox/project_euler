481fcad9efc566b3be10c11b21c594391d78fac1
statox <adrien.fabre92@gmail.com>
2016-12-18 18:56:54 +0100

    Problem 50 remove unfactored code

--- 050_consecutive_prime_sum/main.py
+++ 050_consecutive_prime_sum/main.py
@@ -6,48 +6,10 @@ import sys
 sys.path.insert(0, os.path.abspath('../utils'))
 import utilsEuler
 
-
-
-# IMPORTANT IMPROVED VERSION OF eratosthenesGenerator
-# IT CAN NOW HANDLE A LARGER LIMIT EVEN IN PYTHON 2
-
 # Incredibly long solution: the largest sequence is found pretty quickly
 # but the program should stop much sooner.
 # A new implementation should follow the solution here https://projecteuler.net/thread=50;page=9
 # which is much more efficient
-
-# # Create a generator for the primes numbers under limit
-# # The generator can be used only once
-# def eratosthenesGenerator(limit):
-#     # Generating the list and marking 0 and 1
-#     A = [True for i in range(limit)]
-#     A[0] = A[1] = False
-# 
-#     # Using yield to create a generator containing the prime numbers
-#     for (i, isprime) in enumerate(A):
-#         if isprime:
-#             yield i
-#             for n in xrange(i*i, limit, i):     # Mark factors non-prime
-#                 A[n] = False
-# 
-# def eratosthenesGenerator2(limit):
-#     # Generating the list and marking 0 and 1
-#     A = [True for i in range(limit)]
-#     A[0] = A[1] = False
-# 
-#     # Using yield to create a generator containing the prime numbers
-#     for (i, isprime) in enumerate(A):
-#         if isprime:
-#             yield i
-#             n = i*i - i
-#             while (n<limit-i):
-#             # for n in xrange(i*i, limit, i):     # Mark factors non-prime
-#                 n += i
-#                 A[n] = False
-# 
-# # Returns a list of the primes numbers under limit
-# def eratosthenesList(limit):
-#     return [i for i in eratosthenesGenerator2(limit)]
 
 def main():
 
